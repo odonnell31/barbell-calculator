05a464497010a9cdf472cc840dc5ce9984c92474
Michael ODonnell <36376324+odonnell31@users.noreply.github.com>
2018-03-11 17:56:29 -0400

    Update front squat calc feature
    Update front squat calc features to output more advanced lift plan

--- front_squat_calc.py
+++ front_squat_calc.py
@@ -5,12 +5,15 @@ def front_squat_calc(current_lift, goal_lift, body_weight):
     
     week_num = 1
     while difference > 0.0:
-        print "week:",week_num
+        print "Week:",week_num
         print "5x5 front squat:",(float(current_lift)*0.8)
         print "====="
-        current_lift = current_lift + 2.5
-        difference = difference - 2.5
-        week_num = week_num + 1
+        print "Week:",week_num+1
+        print "5x3 front squat:",(float(current_lift)*0.88)
+        print "====="
+        current_lift = current_lift + 5
+        difference = difference - 5
+        week_num = week_num + 2
         #print int(weeks)
     print "go for it, 1 rep!:",goal_lift
 
