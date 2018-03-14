9154ae8c8f10af8cad54cd62a91151d0bb3f4687
Michael ODonnell <36376324+odonnell31@users.noreply.github.com>
2018-03-13 20:46:16 -0400

    feat: Add print statement
    Add print statement to barbell_calc.py based on if condition.
    
    Add if condition to assess if all weight was accounted for, or if there's weight left over.

--- barbell_calc.py
+++ barbell_calc.py
@@ -5,25 +5,25 @@ def barbell_calc(weight):
     # Divide by 2 for one side of barbell
     if weight >= 45:
         weight = (weight-45)/2.0
-        
+
         Fourty_Five = int(weight)/45
         weight = weight - (45.0*Fourty_Five)
-        
+
         Twenty_Five = int(weight)/25
         weight = weight - (25.0*Twenty_Five)
-        
+
         Ten = int(weight)/10
         weight = weight - (10.0*Ten)
-        
+ 
         Five = int(weight)/5
         weight = weight - (5.0*Five)
-        
+
         Two_Point_Five = weight/2.5
         weight = weight - (2.5*Two_Point_Five)
-        
+
         One_Point_Two_Five = weight/1.25
         weight = weight - (1.25*One_Point_Two_Five)
-        
+
         print "On each side of barbell (in lb's):"
         print "45's:  ", Fourty_Five
         print "25's:  ", Twenty_Five
@@ -31,6 +31,10 @@ def barbell_calc(weight):
         print "5's:   ", Five
         print "2.5's: ", int(Two_Point_Five)
         print "1.25's:", int(One_Point_Two_Five)
+        
+        if weight != 0.0:
+            print "-----"
+            print "there's some weight left over:", weight
         
     elif weight > 0:
         print "hit the dumbbells.."
